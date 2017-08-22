4b3a5631fc787f184baa1c7fd28790ea271b3266
Emerson Santiago <emerson-1806@hotmail.com>
2017-08-22 18:39:28 -0300

    Removing config.php of gitignore

---
+++ config.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    /*
+    | Whether to use HTTPS or not. This defaults to true for security reasons (use HTTPS).
+    | Default: true.
+    | Options: true, false.
+    */
+    "https" => true,
+
+    /*
+    | Use JSON or XML format
+    | Default: "json".
+    | Options: "xml", "json".
+    */
+    "format" => "json",
+
+    /*
+    | Whether or not to skip Disambiguation results
+    | Default: true.
+    | Options: true, false.
+    */
+    "skip_disambiguations" => false,
+
+    /*
+    | Whether to disallow HTML in the result.
+    | Default: false.
+    | Options: true, false.
+    */
+    "html" => false,
+];
